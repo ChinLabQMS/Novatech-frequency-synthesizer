41b54261b3dcc47bb92df813ea8d006897be2a93
zmjm <zmjm@uchicago.edu>
2023-08-25 20:03:30 -0500

    added wrapper decorator for round trip trajectory

--- utils.py
+++ utils.py
@@ -11,11 +11,20 @@ defaultfreq1 = '080.0000000' # frequency when table is not running
 defaultphase1 = '00000' # phase when table is not running
 defaultamp1 = '0820' # amplitude when table is not running (N/1023) default 0820
 
-def freqdiff_flattop(t_inc, t_tot, dist, round_trip, wait_time, tramp=400):
-    if type(tramp) == float:
-        T1 = int(tramp * t_tot / (1e-4*t_inc))
-    elif type(tramp) == int:
-        T1 = tramp
+# Wrapper for round trip trajectory
+def round_trip(func):
+    def inner(wait_time, t_inc, *args, **kwargs):
+        t, f = func(t_inc, *args, **kwargs)
+        f = np.concatenate((f, np.zeros(int(wait_time / (t_inc*1e-4))), -f[::-1]))
+        t = np.arange(len(f), dtype=int)
+        return t, f
+    return inner
+
+def freqdiff_flattop(t_inc, t_tot, t_ramp, dist):
+    if type(t_ramp) == float:
+        T1 = int(t_ramp * t_tot / (1e-4*t_inc))
+    elif type(t_ramp) == int:
+        T1 = t_ramp
 
     t = np.arange(t_tot / (1e-4*t_inc), dtype=int)
     f = (1 - np.cos(np.pi * t / T1)) * (t < T1) \
@@ -23,26 +32,7 @@ def freqdiff_flattop(t_inc, t_tot, dist, round_trip, wait_time, tramp=400):
         + (1 + np.cos(np.pi * (t - (len(t) - T1)) / T1)) * (t >= len(t) - T1)
     
     amp = dist / (np.sum(abs(f)) * wavelength * t_inc * 1e2)
-    if round_trip:
-        f = np.concatenate((f, np.zeros(int(wait_time / (t_inc*1e-4))), -f[::-1]))
-        t = np.arange(len(f), dtype=int)
     return t, np.around(amp * f, decimals=7)
-
-
-def freqdiff_continous(t_inc, t_tot, final_vel, tramp=400):
-    if type(tramp) == float:
-        T1 = int(tramp * t_tot / (1e-4*t_inc))
-    elif type(tramp) == int:
-        T1 = tramp
-    
-    t = np.arange(t_tot / (1e-4*t_inc), dtype=int)
-    f = (1 - np.cos(np.pi * t / T1)) * (t < T1) \
-        + 2 * (t >= T1)
-    
-    amp = final_vel / (f.max() * wavelength * 1e6)
-    f = np.around(amp * f, decimals=7)
-
-    return t, f
 
 def make_figure(freqdiff, t_inc, trajName):
     dist_actual = np.cumsum(freqdiff) * wavelength * t_inc *1e2
@@ -62,14 +52,9 @@ def make_figure(freqdiff, t_inc, trajName):
     ax.legend(['freq1 - freq0'])
     plt.show()
 
-    return fig, dist_actual
+    return fig
 
 def save_table(freqdiff, t_inc, trajName, fig):
-    amp0 = [defaultamp0] * len(freqdiff)
-    amp1 = [defaultamp1] * len(freqdiff)
-    phase0 = [defaultphase0] * len(freqdiff)
-    phase1 = [defaultphase1] * len(freqdiff)
-    freq0 = [defaultfreq0] * len(freqdiff)
     freq1 = ["0%#10.7f" % (x + float(defaultfreq1)) for x in (freqdiff)]
 
     if os.path.exists('./' + datetime.now().strftime('%Y-%m-%d')) == False:
@@ -78,14 +63,22 @@ def save_table(freqdiff, t_inc, trajName, fig):
     currentwdr = os.getcwd()
     fileName = datetime.now().strftime('%Y-%m-%d_%H-%M-%S_') + trajName + '.txt' # Output file name
 
+    counter = 1
+    prevfreq1 = defaultfreq1
     with open(currentwdr+'\\'+datetime.now().strftime('%Y-%m-%d')+'\\'+fileName,'w+') as textFile:
         textFile.write('0 ' + defaultfreq0 + ' ' + defaultphase0 + ' ' + defaultamp0 + ' 0255\n')
         textFile.write('1 ' + defaultfreq1 + ' ' + defaultphase1 + ' ' + defaultamp1 + ' 0255\n')
 
-        for i in range(len(freq0)):
-            textFile.write('0 ' + freq0[i] + ' ' + phase0[i] + ' ' + amp0[i] + ' ' + format(t_inc,"04") + '\n')
-            textFile.write('1 ' + freq1[i] + ' ' + phase1[i] + ' ' + amp1[i] + ' ' + format(t_inc,"04") + '\n')
-        
+        for i in range(len(freq1)):
+            if freq1[i] != prevfreq1:
+                prevfreq1, counter = freq1[i], 1
+            else:
+                counter += 1
+                continue
+            textFile.write('0 ' + defaultfreq0 + ' ' + defaultphase0 + ' ' + defaultamp0 + ' ' + format(t_inc * counter,"04") + '\n')
+            textFile.write('1 ' + prevfreq1 + ' ' + defaultphase1 + ' ' + defaultamp1 + ' ' + format(t_inc * counter,"04") + '\n')
+        textFile.write('0 ' + defaultfreq0 + ' ' + defaultphase0 + ' ' + defaultamp0 + format(t_inc * counter,"04") + '\n')
+        textFile.write('1 ' + prevfreq1 + ' ' + defaultphase1 + ' ' + defaultamp1 + format(t_inc * counter,"04") + '\n')
         textFile.write('0 ' + defaultfreq0 + ' ' + defaultphase0 + ' ' + defaultamp0 + ' 0000\n')
         textFile.write('1 ' + defaultfreq1 + ' ' + defaultphase1 + ' ' + defaultamp1 + ' 0000')
 
